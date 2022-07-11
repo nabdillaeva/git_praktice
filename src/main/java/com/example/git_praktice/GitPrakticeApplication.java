1318076506c327c04b290bcfbd4684347109931b
AnarbaevErbol <anarbaeverik96@gmail.com>
2022-07-11 08:29:49 +0300

    bolboytko

--- src/main/java/com/example/git_praktice/GitPrakticeApplication.java
+++ src/main/java/com/example/git_praktice/GitPrakticeApplication.java
@@ -11,8 +11,6 @@ public class GitPrakticeApplication {
 
         for (int i = 0; i < 20; i++) {
             System.out.println("Ulan");
-
-            System.out.println("gadgetarium rulit");
         }
 
         System.out.println("gadgetarium rulit");
