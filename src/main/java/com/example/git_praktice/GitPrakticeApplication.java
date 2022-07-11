49be514f24a2b7bbd3c345eaf300e5dc1f50ef91
nabdillaeva <nuriza.abdillaeva.ch@gmail.com>
2022-07-11 10:27:11 +0600

    first commit

---
+++ src/main/java/com/example/git_praktice/GitPrakticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.git_praktice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPrakticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitPrakticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/git_praktice/GitPrakticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_praktice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPrakticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
